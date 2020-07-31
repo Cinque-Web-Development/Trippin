afe02e77b7b358ec2ee979c6dd0a674850e70ac8
Samuel Trahan <samtrahan91@gmail.com>
2020-07-31 13:02:41 -0500

    jwt auth

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/App/App';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Route render={({history}) => <App history={history} />} />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
