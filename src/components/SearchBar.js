e5a4c38f00fde797e157c50cac652fb6c96c5f18
Samuel Trahan <samtrahan91@gmail.com>
2020-08-24 10:10:04 -0500

    added changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = ({term, onInputChange}) => {
+const SearchBar = ({ term, onInputChange }) => {
   return (
     <div>
       <div className="ui form">
-          <div className="field">
-            <label>Search City</label>
-            <input value={term} onChange={onInputChange} className="input" type="text" />
-            <button className="ui green button">Search</button>
-          </div>
+        <div className="field">
+          <label>Search City</label>
+          <input
+            value={term}
+            onChange={onInputChange}
+            className="input"
+            type="text"
+          />
+          <button className="ui green button">Search</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
