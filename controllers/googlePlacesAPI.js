710c08d43f76c04d6d6d7c707a81b38b599a6650
Samuel Trahan <samtrahan91@gmail.com>
2020-09-03 18:37:01 -0500

    api working i think

--- controllers/googlePlacesAPI.js
+++ controllers/googlePlacesAPI.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-const BASE_URL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/`
+const BASE_URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?`
 
 module.exports = {
     searchCities
 }
 
 function searchCities(req, res) {
-    axios.get(`${BASE_URL}json?key=${process.env.REACT_APP_GOOGLE_API}&input=${req.body.term}`)
+    axios.get(`${BASE_URL}json?key=${process.env.REACT_APP_GOOGLE_API}&query=${req.body.term}`)
     .then(response => {res.json(response)})
     .catch(err => console.log(err))
 }
