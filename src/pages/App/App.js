3ff9ef464f7e1500490e1cf0ee2516a8c1b7d509
Samuel Gemberling <sgembeling@gmail.com>
2020-09-03 19:35:59 -0500

    commit for merge

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,18 +3,19 @@ import { Route, Switch } from 'react-router-dom'
 
 import './App.css';
 
+import userService from '../../services/userService'
 
 import ErrorPage from '../ErrorPage/ErrorPage'
-import GoogleLoginButton from '../../components/GoogleLoginButton'
 
 import Layout from '../../components/Layout/Layout';
 import StartTrip from '../../components/StartTrip/StartTrip'
+import GoogleLoginButton from '../../components/GoogleLoginButton'
 import ErrorBoundary from '../../components/ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
-
-
-
+  state = {
+    user: null
+  }
 
   render () {
     return (
