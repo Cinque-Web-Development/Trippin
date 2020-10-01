312e3b0047b779e9537f268d074f15d6eb8dbb7a
Samuel Trahan <samtrahan91@gmail.com>
2020-10-01 09:34:22 -0500

    autocomplete city search

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React, { useState } from "react";
 import NavBar from "./components/NavBar";
-import SearchBar from "./components/SearchBar";
+
+import SearchLocationInput from "./components/SearchLocationInput";
 import "./App.css";
-import axios from "axios";
 
 const App = () => {
   const [cities, setCities] = useState([]);
 
-  const handleSearchSubmit = async (event, searchCity) => {
-    event.preventDefault();
-    axios
-      .get("/cities", {
-        params: {
-          searchCity: searchCity,
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-        setCities(response.data.results);
-      })
-      .catch((err) => console.log(err));
-  };
-
   return (
     <>
       <NavBar />
-      <div className="body">
-        <SearchBar handleSearchSubmit={handleSearchSubmit} />
-      </div>
+      <SearchLocationInput />
     </>
   );
 };
