b46ec563e6c2cd24003203e1a3da23035993fdf6
Samuel Trahan <samtrahan91@gmail.com>
2020-10-06 11:44:24 -0500

    fake images and descriptions for cities

--- src/components/CityDetails/CityDetails.js
+++ src/components/CityDetails/CityDetails.js
@@ -1,32 +1,35 @@
-import React, {useState, useEffect}from 'react';
-import Amenities from '../Amenities/Amenities';
+import React, { useState, useEffect } from "react";
+import Amenities from "../Amenities/Amenities";
+import faker from "faker";
 
-import './CityDetails.css'
+import "./CityDetails.css";
 
-const CityDetails = ({hotels, city, restaurants}) => {
-  const [hotel, setHotel] = useState([])
-  const [restaurant, setRestaurant] = useState([])
-  
+const CityDetails = ({ hotels, city, restaurants }) => {
+  const [hotel, setHotel] = useState([]);
+  const [restaurant, setRestaurant] = useState([]);
+
   useEffect(() => {
-    const getHotel = hotels.slice(0, 5)
+    const getHotel = hotels.slice(0, 5);
     const getRestaurant = restaurants.slice(0, 5);
-    setRestaurant(getRestaurant)
-    setHotel(getHotel)
-  }, [hotels, restaurants])
+    setRestaurant(getRestaurant);
+    setHotel(getHotel);
+  }, [hotels, restaurants]);
 
   return (
     <>
       <h1 className="city-name">{city}</h1>
+      <img className="city-detail-img" alt="" src={faker.image.city()}></img>
+      <p className="city-description">{faker.lorem.paragraph()}</p>
       <div className="amenity-wrapper">
         <h2 className="amenity-title">Hotels</h2>
-        <Amenities amenities={hotel}/>
+        <Amenities amenities={hotel} />
       </div>
       <div className="amenity-wrapper">
         <h2 className="amenity-title">Restaurants</h2>
-        <Amenities amenities={restaurant}/>
+        <Amenities amenities={restaurant} />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CityDetails
+export default CityDetails;
