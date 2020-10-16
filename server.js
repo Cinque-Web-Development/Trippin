a33a48d792e18868ef5778bbdf2b1a21b736a2df
Samuel Trahan <samtrahan91@gmail.com>
2020-10-16 16:13:11 -0500

    google geocode api call made

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 require("dotenv").config()
 require('./config/database')
+const axios = require('axios');
 
 const authRouter = require('./routes/auth')
 const tripRouter = require('./routes/trips')
@@ -16,6 +17,14 @@ app.use(logger('dev'))
 
 app.use('/api/auth/', authRouter)
 app.use('/api/trips/', tripRouter)
+
+app.use('/citycoord', (req, res) => {
+  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.city}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((err) => console.log(err));
+})
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
