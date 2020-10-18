6445f8aba596dbfc11ad6e8f2085768445449882
Samuel Trahan <samtrahan91@gmail.com>
2020-10-18 14:36:20 -0500

    start trip page styling some what

--- src/components/GoogleMaps/GoogleMaps.js
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps'
-
+import './GoogleMaps.css'
 
 
 
@@ -19,7 +19,7 @@ export default function GoogleMaps({lat, lng}) {
     
     const WrappedMap = withScriptjs(withGoogleMap(Map));
   return (
-    <div style={{width: '80vw', height: '100vh'}}>
+    <div  style={{width: '80vw', height: '100vh'}}>
         <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`}
         loadingElement={<div style={{height: '100%'}} />}
         containerElement={<div style={{height: '400px'}} />}
