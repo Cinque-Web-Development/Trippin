62f31214d83c0455f8b41d745d2ec815021e589d
Samuel Gemberling <sgembeling@gmail.com>
2020-10-26 10:15:07 -0500

    change history

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './pages/App/App'
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
     <React.StrictMode>
-      <Route render={({history}) => <App history={history} />} />
+      <App />
     </React.StrictMode>
   </Router>,
   document.getElementById('root')
